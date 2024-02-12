8aa5c47f3eff88658558cd2ff5e0d6b441ffd8d8
Sergey Getmanskiy <getmansky1997@yandex.ru>
2024-02-12 17:35:07 +0300

    initialize project

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { CssBaseline } from '@mui/material'
+import './App.css'
+
+function App() {
+
+  return (
+    <React.Fragment>
+      <CssBaseline />
+      {/* The rest of your application */}
+    </React.Fragment>
+  )
+}
+
+export default App
