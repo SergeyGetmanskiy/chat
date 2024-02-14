d79fcd3cb865b89c1659b4f1a35added28d06993
Sergey Getmanskiy <getmansky1997@yandex.ru>
2024-02-14 10:57:25 +0300

    fix eslint

--- src/components/Rings/Rings.tsx
+++ src/components/Rings/Rings.tsx
@@ -5,7 +5,7 @@ function Rings({children}: {children: ReactNode}) {
 
 return (
   <div className='ring'>
-    <i style={{[--clr]:"#00ff0a"}} />
+    <i style={{"--clr":"#00ff0a"}} />
     <i style={{"--clr":"#ff0057"}} />
     <i style={{"--clr":"#fffd44"}} />
     {children}
