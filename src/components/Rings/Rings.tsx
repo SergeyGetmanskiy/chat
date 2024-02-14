557cef8c1189bfae38b2f64ee8455f8a4deef174
Sergey Getmanskiy <getmansky1997@yandex.ru>
2024-02-14 11:24:41 +0300

    fix eslint

--- src/components/Rings/Rings.tsx
+++ src/components/Rings/Rings.tsx
@@ -7,7 +7,9 @@ return (
   <div className='ring'>
     {/* @ts-expect-error --clr variable does not exist in type 'Properties<string | number, string & {}>' */}
     <i style={{"--clr":"#00ff0a"}} />
+    {/* @ts-expect-error --clr variable does not exist in type 'Properties<string | number, string & {}>' */}
     <i style={{"--clr":"#ff0057"}} />
+    {/* @ts-expect-error --clr variable does not exist in type 'Properties<string | number, string & {}>' */}
     <i style={{"--clr":"#fffd44"}} />
     {children}
   </div>
