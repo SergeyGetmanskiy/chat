f89134e6ab0356ea018410267b18179abed9352f
Sergey Getmanskiy <getmansky1997@yandex.ru>
2024-02-14 14:55:40 +0300

    compose auth form

--- src/pages/SignIn/Signin.tsx
+++ src/pages/SignIn/Signin.tsx
@@ -1,4 +1,6 @@
-import { Paper, Stack, TextField, Typography } from '@mui/material'
+import { Paper, Stack, Typography, Button } from '@mui/material'
+import { FormContainer, TextFieldElement } from 'react-hook-form-mui'
+
 import Background from '../../components/Background/Background'
 import Rings from '../../components/Rings/Rings'
 
@@ -8,24 +10,31 @@ function Signin() {
     <Background>
       <Stack sx={{display: 'flex', height: '100%', alignItems: 'center', justifyContent: 'center'}}>
         <Rings>
-          <Paper elevation={12} sx={{display: 'flex', flexDirection: 'column', rowGap: '10px', p: '20px'}}>
-            <Typography align='center' variant='h4'>
-              Вход
-            </Typography>
-            <TextField
-              id="userId"
-              type='text'
-              label="User name"
-              size='small'
-              required
-            />
-            <TextField
-              id="userId"
-              type='text'
-              label="Password"
-              size='small'
-              required
-            />
+          <Paper elevation={12} sx={{display: 'flex', flexDirection: 'column', rowGap: '10px', p: '30px'}}>
+            <FormContainer>
+              <Typography align='center' variant='h4'>
+                Вход
+              </Typography>
+              <br />
+              <Stack direction={'column'}>
+                <TextFieldElement
+                  name={'username'}
+                  label={'Имя пользователя'}
+                />
+                <br />
+                <TextFieldElement
+                  name={'password'}
+                  label={'Пароль'}
+                />
+                <br />
+                <Button type={'submit'} variant={'contained'} color={'secondary'}>
+                  Войти
+                </Button>
+              </Stack>
+            </FormContainer>
+            <br />
+            <Typography align='center'>Ещё не зарегистрированы?</Typography>
+            <Button href='/sign-up' color='secondary'>Зарегистрироваться</Button>
           </Paper>
         </Rings>
       </Stack>
