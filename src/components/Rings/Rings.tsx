f63cd2bb0529bc0287756a42927aec9f47275400
Sergey Getmanskiy <getmansky1997@yandex.ru>
2024-02-14 10:55:06 +0300

    fix eslint

--- src/components/Rings/Rings.tsx
+++ src/components/Rings/Rings.tsx
@@ -5,11 +5,8 @@ function Rings({children}: {children: ReactNode}) {
 
 return (
   <div className='ring'>
-    {/*eslint-disable-next-line*/}
     <i style={{[--clr]:"#00ff0a"}} />
-    {/*eslint-disable-next-line*/}
     <i style={{"--clr":"#ff0057"}} />
-    {/*eslint-disable-next-line*/}
     <i style={{"--clr":"#fffd44"}} />
     {children}
   </div>
