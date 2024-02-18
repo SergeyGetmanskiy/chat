e3f3d39630a30e8b5f815a397146d369ba826a7c
Sergey Getmanskiy <getmansky1997@yandex.ru>
2024-02-18 13:13:05 +0300

    skip window reload

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import { Routes, Route, useNavigate, useLocation } from 'react-router-dom'
 import Signin from './pages/SignIn/Signin'
 import Signup from './pages/Signup/Signup'
-import { useEffect, useState } from 'react'
+import { useEffect } from 'react'
+import Main from './pages/Main/Main';
 
 
 function App() {
@@ -13,11 +14,12 @@ function App() {
     if(pathname == '/') {
       navigate('/signin')
     }
-  }, [])
+  }, [pathname, navigate])
 
   return (
     <div>
       <Routes>
+        <Route path="/" element={<Main />} />
         <Route path="/signin" element={<Signin />} />
         <Route path="/signup" element={<Signup />} />
       </Routes>
